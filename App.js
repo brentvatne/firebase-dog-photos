b4b72f47e6f72e6831c0283cab9ebe7f38947718
Brent Vatne <brentvatne@gmail.com>
2020-08-05 17:48:52 -0700

    Can add images now

--- App.js
+++ App.js
@@ -1,15 +1,16 @@
 import { StatusBar } from "expo-status-bar";
-import React, { useEffect, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import {
   Button,
+  Dimensions,
   Image,
   ScrollView,
   StyleSheet,
   Text,
+  TextInput,
   View,
-  useWindowDimensions,
 } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
+import { SafeAreaView, SafeAreaProvider } from "react-native-safe-area-context";
 import uniqBy from "lodash/uniqBy";
 import * as firebase from "firebase";
 import "firebase/firestore";
@@ -32,7 +33,15 @@ const auth = firebase.auth();
 const db = firebase.firestore();
 const corgisCol = db.collection("corgis");
 
-export default function App() {
+export default function AppContainer() {
+  return (
+    <SafeAreaProvider>
+      <App />
+    </SafeAreaProvider>
+  );
+}
+
+function App() {
   const [user, setUser] = React.useState(null);
   useEffect(() => {
     return auth.onAuthStateChanged((newUser) => {
@@ -42,25 +51,28 @@ export default function App() {
 
   const [corgis, setCorgis] = React.useState(null);
   useEffect(() => {
-    corgisCol.onSnapshot((snapshot) => {
-      const allCorgis = snapshot.docs.map((doc) => ({
-        id: doc.id,
-        ...doc.data(),
-      }));
+    return corgisCol
+      .orderBy("createdAt", "desc")
+      .limit(10)
+      .onSnapshot((snapshot) => {
+        const allCorgis = snapshot.docs.map((doc) => ({
+          id: doc.id,
+          ...doc.data(),
+        }));
 
-      const filteredCorgis = allCorgis.filter(
-        (corgi) =>
-          corgi.url && corgi.url.startsWith("https://images.unsplash.com")
-      );
-      const dedupedCorgis = uniqBy(filteredCorgis, "url");
-      const mostRecentCorgis = dedupedCorgis.slice(0, 20);
-      setCorgis(mostRecentCorgis);
-    });
-  });
+        const filteredCorgis = allCorgis.filter(
+          (corgi) =>
+            corgi.url && corgi.url.startsWith("https://images.unsplash.com")
+        );
+        const dedupedCorgis = uniqBy(filteredCorgis, "url");
+        const mostRecentCorgis = dedupedCorgis.slice(0, 20);
+        setCorgis(mostRecentCorgis);
+      });
+  }, []);
 
   return (
     <SafeAreaView style={styles.container} edges={["top"]}>
-      <ScrollView style={{ flex: 1 }}>
+      <ScrollView style={{ flex: 1 }} keyboardShouldPersistTaps="always">
         <View
           style={{
             flexDirection: "row",
@@ -85,6 +97,8 @@ export default function App() {
             }}
           />
         </View>
+        {user ? <NewCorgiForm /> : null}
+
         {corgis ? <CorgiList corgis={corgis} /> : <Text>No corgis 😧</Text>}
         <StatusBar style="auto" />
       </ScrollView>
@@ -92,9 +106,49 @@ export default function App() {
   );
 }
 
-function CorgiList({ corgis }) {
-  const dimensions = useWindowDimensions();
+function NewCorgiForm() {
+  const [corgiUrl, setCorgiUrl] = useState(null);
+  const handleAddCorgi = useCallback(async () => {
+    const user = auth.currentUser;
+    if (!user) {
+      return;
+    }
 
+    try {
+      await corgisCol.add({
+        url: corgiUrl,
+        uid: user.uid,
+        createdAt: new Date(),
+      });
+    } catch (e) {
+      alert(e.message);
+    }
+    setCorgiUrl(null);
+  }, [corgiUrl]);
+
+  return (
+    <View
+      style={{
+        flexDirection: "row",
+        justifyContent: "space-between",
+        paddingHorizontal: 10,
+        paddingVertical: 5,
+      }}
+    >
+      <TextInput
+        placeholder="Add a Corgi with the Unsplash URL"
+        value={corgiUrl}
+        onChangeText={(text) => setCorgiUrl(text)}
+        onSubmitEditing={handleAddCorgi}
+        clearButtonMode="while-editing"
+        style={{ flex: 1 }}
+      />
+      <Button title="Add Corgi" onPress={handleAddCorgi} />
+    </View>
+  );
+}
+
+function CorgiList({ corgis }) {
   return (
     <View style={{ flexDirection: "row", flex: 1, flexWrap: "wrap" }}>
       {corgis.map((corgi) => (
@@ -102,8 +156,8 @@ function CorgiList({ corgis }) {
           key={corgi.url}
           source={{ uri: corgi.url }}
           style={{
-            width: dimensions.width / 3,
-            height: dimensions.width / 3,
+            width: Dimensions.get("window").width / 3,
+            height: Dimensions.get("window").width / 3,
             resizeMode: "cover",
           }}
           accessibilityLabel="Corgi"
