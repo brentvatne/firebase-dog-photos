102e0f91346950f2e400600b6d56ba84773683cc
Brent Vatne <brentvatne@gmail.com>
2020-08-10 18:52:15 -0700

    add modal!

--- App.js
+++ App.js
@@ -1,13 +1,17 @@
 import React, { useCallback, useEffect, useState } from "react";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 import {
   ActivityIndicator,
   Button,
   Dimensions,
   Image,
+  Modal,
   ScrollView,
   StyleSheet,
+  Platform,
   Text,
   TextInput,
+  TouchableOpacity,
   View,
 } from "react-native";
 import { StatusBar } from "expo-status-bar";
@@ -29,7 +33,11 @@ function App() {
 
   return (
     <SafeAreaView style={styles.container} edges={["top"]}>
-      <ScrollView style={{ flex: 1 }} keyboardShouldPersistTaps="always" keyboardDismissMode="on-drag">
+      <ScrollView
+        style={{ flex: 1 }}
+        keyboardShouldPersistTaps="always"
+        keyboardDismissMode="on-drag"
+      >
         <View
           style={{
             flexDirection: "row",
@@ -38,7 +46,15 @@ function App() {
             paddingVertical: 5,
           }}
         >
-          <Text style={{ fontSize: 25 }}>corgislist</Text>
+          <View style={{ flexDirection: "row", alignItems: "center" }}>
+            <MaterialCommunityIcons
+              name="dog-side"
+              size={25}
+              color="black"
+              style={{ marginRight: 5 }}
+            />
+            <Text style={{ fontSize: 25 }}>corgislist</Text>
+          </View>
           <Button
             title={user ? "Sign out" : "Sign in anonymously"}
             onPress={() => {
@@ -76,6 +92,9 @@ function NewCorgiForm() {
         justifyContent: "space-between",
         paddingHorizontal: 10,
         paddingVertical: 5,
+        borderTopWidth: 1,
+        borderBottomWidth: 1,
+        borderColor: "rgba(0,0,0,0.1)",
       }}
     >
       <TextInput
@@ -93,8 +112,9 @@ function NewCorgiForm() {
 
 function CorgiList({ corgis }) {
   const {
-    window: { width },
+    window: { width, height },
   } = useDimensions();
+  const [selectedCorgi, setSelectedCorgi] = useState(null);
 
   if (!corgis) {
     if (corgis === null) {
@@ -107,17 +127,56 @@ function CorgiList({ corgis }) {
   return (
     <View style={{ flexDirection: "row", flex: 1, flexWrap: "wrap" }}>
       {corgis.map((corgi) => (
-        <Image
+        <TouchableOpacity
+          onPress={() => setSelectedCorgi(corgi)}
           key={corgi.url}
-          source={{ uri: corgi.url }}
-          style={{
-            width: width / 3,
-            height: width / 3,
-            resizeMode: "cover",
-          }}
-          accessibilityLabel="Corgi"
-        />
+        >
+          <Image
+            source={{ uri: corgi.url }}
+            style={{
+              width: width / 3,
+              height: width / 3,
+              resizeMode: "cover",
+            }}
+            accessibilityLabel="Corgi"
+          />
+        </TouchableOpacity>
       ))}
+
+      <Modal transparent={true} visible={!!selectedCorgi}>
+        {selectedCorgi ? (
+          <View
+            style={{
+              flex: 1,
+              alignItems: "center",
+              justifyContent: "center",
+              backgroundColor: "rgba(0,0,0,0.8)",
+              // React Native's Modal component does not yet work on web,
+              // so we can get around that by using fixed position on web
+              ...Platform.select({
+                web: {
+                  position: "fixed",
+                  top: 0,
+                  left: 0,
+                  right: 0,
+                  bottom: 0,
+                },
+              }),
+            }}
+          >
+            <TouchableOpacity onPress={() => setSelectedCorgi(null)}>
+              <Image
+                source={{ uri: selectedCorgi.url }}
+                style={{
+                  width: width > height ? height : width,
+                  height: width > height ? height : width,
+                  resizeMode: "contain",
+                }}
+              />
+            </TouchableOpacity>
+          </View>
+        ) : null}
+      </Modal>
     </View>
   );
 }
